c56d26baf37d1665d913d8dede7d59dc8ed3bfcc
Siabell <valesiabatto@gmail.com>
2020-03-05 15:37:30 -0500

    Creacion repositorio backend
    Co-authored-by: Siabell <valesiabatto@gmail.com>
    Co-authored-by: acai-bjca <amalia.alfonso@mail.escuelaing.edu.co>
    Co-authored-by: AnVillab99 <andresvilla990223@gmail.com>
    Co-authored-by: swilsonmelo <swilsonmelo@gmail.com>

---
+++ src/main/java/eci/ieti/proyecto/voiceBox_backend/VoiceBoxBackendApplication.java
@@ -0,0 +1,13 @@
+package eci.ieti.proyecto.voiceBox_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoiceBoxBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoiceBoxBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eci/ieti/proyecto/voiceBox_backend/VoiceBoxBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package eci.ieti.proyecto.voiceBox_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoiceBoxBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

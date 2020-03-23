b6d5d225246d29e09ae069f78c1141e0373a4bf7
Andres Villamil <andresvilla990223@gmail.com>
2020-03-23 17:34:11 -0500

    cambios minimos (ignorabl)

--- src/main/java/ieti/voicebox/service/CommentsService.java
+++ src/main/java/ieti/voicebox/service/CommentsService.java
@@ -1,8 +1,6 @@
 package ieti.voicebox.service;
 
 import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 
